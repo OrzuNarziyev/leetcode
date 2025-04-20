33029bed09ad0b7a2ca55b9b030411fe25048dc7
OrzuNarziyev <narziyevorzu@gmail.com>
2025-04-20 19:40:07 +0500

    add: 781. Problem solution Rabbits in forest

---
+++ rabbits_in_forest/src/main.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
+pub struct Solution;
+
+
+impl Solution {
+    pub fn num_rabbits(answers: Vec<i32>) -> i32 {
+        answers.iter().fold(
+            HashMap::<&i32, i32>::new(), |mut m, i| {
+                *m.entry(&i).or_insert(0) += 1;
+                m
+            }).into_iter().map(|(k, v)| ((k + v) / (k + 1)) * (k + 1)).sum()
+    }
+}
+fn main() {
+    let _result = Solution::counter(vec![1, 1, 2]);
+}
